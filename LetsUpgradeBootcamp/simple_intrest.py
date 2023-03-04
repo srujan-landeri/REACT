e6aa4e7cb94ee77fb621428b28c13393fab233f2
Landeri Srujan <66351075+srujan-landeri@users.noreply.github.com>
2023-03-04 20:19:52 +0530

    Add files via upload

---
+++ LetsUpgradeBootcamp/simple_intrest.py
@@ -0,0 +1,8 @@
+# program for calculating simple intrest
+
+p = int(input("Enter Pricipal amount"))
+t = int(input("Enter Time period"))
+r = int(input("Enter rate of intrest"))
+
+print("Simple intrest:", (p*t*r)/100)
+
