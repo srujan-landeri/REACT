05d62a243fddb12787e4139bcf76cc12c90be1e4
Landeri Srujan <66351075+srujan-landeri@users.noreply.github.com>
2023-03-10 10:21:48 +0530

    Delete LetsUpgradeBootcamp directory

--- LetsUpgradeBootcamp/simple_intrest.py
+++
@@ -1,8 +0,0 @@
-# program for calculating simple intrest
-
-p = int(input("Enter Pricipal amount"))
-t = int(input("Enter Time period"))
-r = int(input("Enter rate of intrest"))
-
-print("Simple intrest:", (p*t*r)/100)
-
